bbbcfad49207cf91763b04806897d01c9e25a396
HISASHI FUJISATO <>
2024-01-31 10:26:13 +0900

    Rails7のimportmapを導入

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link boards/index.js//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
